07213ba33f0f3594b8204fc92c2df946fa27a11b
Brian Procopio <brian.procopio@gmail.com>
2024-03-20 19:25:19 -0400

    Abandoning... for now

---
+++ README.md
@@ -0,0 +1,7 @@
+# spss.nvim
+
+## Description
+
+This was a fork from [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim). See the original README there.
+
+After digesting and understanding lua and neovim configurations better (thanks to kickstart), I started to modify the original kickstart.nvim configuration to my own liking, adding few plugins and modifying the configuration to my own taste. The more I added and the more I found myself looking at other configurations, the more I realized how unbelievable [LazyVim](https://github.com/LazyVim/LazyVim) is. It's a configuration that is so well thought out and so well documented that I decided to abandon this and use that as a base for my own configuration instead. I will keep this repository for reference and for anyone who wants to use it as a starting point for their own configuration.
---
+++ lua/options.lua
@@ -0,0 +1,73 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, for help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Set all encoding to UTF-8
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+-- Set whitespace options
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.wrap = false
+vim.opt.backspace = { 'indent', 'eol', 'start' }
+vim.opt.scrolloff = 10
+
+-- Ignored when searching
+vim.opt.wildignore = { '*.swp', '*.tmp', '*.bak', '*.git', '*.DS_Store', '*.npm', '*.yarn', '*/node_modules/*' }
